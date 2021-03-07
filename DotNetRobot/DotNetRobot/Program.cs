17a00e03f95d9a81297f226b78ed4b05348539e4
MattSpeakman <m.speakman89@googlemail.com>
2021-03-07 23:21:11 +0000

    Reinitiialised solution using VS

---
+++ DotNetRobot/DotNetRobot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace DotNetRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using(var robot = new Robot())
+            {
+                robot.Forwards();
+                Thread.Sleep(1000);
+
+                robot.Backwards();
+                Thread.Sleep(1000);
+                
+                robot.Right();
+                Thread.Sleep(1000);
+                
+                robot.Left();
+                Thread.Sleep(1000);
+                
+                robot.Stop();
+            }
+        }
+    }
+}
